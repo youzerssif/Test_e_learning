491c7f9570d2fb0a0e854f2a5d10c1f7752a9bb5
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2021-05-11 22:55:11 +0200

    add template

--- COULI/courseapp/urls.py
+++ COULI/courseapp/urls.py
@@ -3,8 +3,15 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
-app_name = 'myapp'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('connexion', views.connexion, name='connexion'),
+    path('chapitre', views.chapitre, name='chapitre'),
+    path('detailchapitre', views.detailChapitre, name='detailChapitre'),
+    
+    ################## Admin dashboard ###########
+    path('indexAdmin', views.indexAdmin, name='indexAdmin'),
+    path('addAdmin', views.addAdmin, name='addAdmin'),
+    path('addChap', views.addChap, name='addChap'),
 
 ]
--- COULI/courseapp/views.py
+++ COULI/courseapp/views.py
@@ -7,3 +7,51 @@ def home(request):
 
     data = {}
     return render(request, 'index.html', data)
+
+
+
+def connexion(request):
+
+    data = {}
+    return render(request, 'connexion.html', data)
+
+
+def chapitre(request):
+    
+    data = {
+        # 'slug':slug,
+    }
+    return render(request, 'chapitre.html', data)
+
+
+def detailChapitre(request):
+    
+    data = {
+        # 'slug':slug,
+    }
+    return render(request, 'detail-chapitre.html', data)
+
+
+############################## Dashboard views #################
+
+def indexAdmin(request):
+    
+    data = {
+        # 'slug':slug,
+    }
+    return render(request, 'index-admin.html', data)
+
+
+def addAdmin(request):
+    
+    data = {
+        # 'slug':slug,
+    }
+    return render(request, 'add-cours.html', data)
+
+def addChap(request):
+    
+    data = {
+        # 'slug':slug,
+    }
+    return render(request, 'add-chapitre.html', data)
